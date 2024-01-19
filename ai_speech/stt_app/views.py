8824a422230e07df192bfcf3440bf674e7d58123
huseyinozkan26 <ozkanhuseyin@yandex.com.tr>
2024-01-19 13:05:40 +0300

    req updated

--- ai_speech/stt_app/views.py
+++ ai_speech/stt_app/views.py
@@ -10,9 +10,6 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.views import View
-import shutil
-import uuid
-from django.conf import settings
 
 class SpeechRecognitionView(APIView):
     @csrf_exempt
